7b9b6ab52adfc3e8f96688add3662b2ddf4ebeaf
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-10-23 19:52:30 +0200

    playlist

--- playlist-contains-song-selled.sql
+++ playlist-contains-song-selled.sql
@@ -1,21 +1,15 @@
-WITH SoldTwice AS (
-    SELECT
-        pt.PlaylistId,
-        il.TrackId
-    FROM invoice_items il
-    INNER JOIN playlist_track pt ON il.TrackId = pt.TrackId
-    WHERE il.Quantity >= 2
-)
-
 SELECT
     p.PlaylistId,
     p.Name,
-    ROUND(COALESCE(
-        (CAST(COUNT(st.TrackId) AS REAL) / CAST(COUNT(pt.TrackId) AS REAL)) * 100,
-        0
-    ), 4) AS "% song selled twice"
+    ROUND(
+        COALESCE(
+            (CAST(COUNT(DISTINCT il.TrackId) AS REAL) / COUNT(DISTINCT pt.TrackId)) * 100,
+            0
+        ),
+        4
+    ) AS "% song selled twice"
 FROM playlists p
-LEFT JOIN SoldTwice st ON p.PlaylistId = st.PlaylistId
 LEFT JOIN playlist_track pt ON p.PlaylistId = pt.PlaylistId
+LEFT JOIN invoice_items il ON pt.TrackId = il.TrackId AND il.Quantity >= 2
 GROUP BY p.PlaylistId, p.Name
 ORDER BY p.PlaylistId;
