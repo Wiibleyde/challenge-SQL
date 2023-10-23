70419338183032a810cccbc38c669e1c3eff9ade
Wiibleyde <nathan@bonnell.fr>
2023-10-23 13:48:14 +0200

    count support fix

--- support-count-customers.sql
+++ support-count-customers.sql
@@ -1,3 +1,3 @@
-SELECT employees.FirstName, employees.LastName || ' ' || UPPER(employees.LastName) AS FullName, COUNT(customers.CustomerId) AS NumberOfCustomers FROM employees
+SELECT employees.LastName || ' ' || UPPER(employees.LastName) AS FullName, COUNT(customers.CustomerId) AS NumberOfCustomers FROM employees
 LEFT JOIN customers ON employees.EmployeeId = customers.SupportRepId
 WHERE employees.Title = 'Sales Support Agent' GROUP BY employees.EmployeeId ORDER BY NumberOfCustomers ASC;
