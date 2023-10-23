a127355706fce68bb950b6bb92768b971d464052
Wiibleyde <nathan@bonnell.fr>
2023-10-23 15:02:40 +0200

    invoice items

---
+++ invoices-items-last.sql
@@ -0,0 +1,10 @@
+SELECT Tracks.Name AS ProductName
+FROM Tracks
+JOIN (
+    SELECT InvoiceId
+    FROM Invoices
+    ORDER BY InvoiceDate DESC
+    LIMIT 1
+) AS LastInvoice
+ON LastInvoice.InvoiceId = invoice_items.InvoiceId
+JOIN invoice_items ON invoice_items.TrackId = Tracks.TrackId;
