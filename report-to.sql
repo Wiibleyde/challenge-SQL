fe1139908051dc16b4b86f8fe91e2c4320d6ad1e
Wiibleyde <nathan@bonnell.fr>
2023-10-23 16:28:25 +0200

    erport to

---
+++ report-to.sql
@@ -0,0 +1,6 @@
+SELECT
+    E1.EmployeeId AS EmployeeId,
+    E1.FirstName || ' ' || E1.LastName AS EmployeeName,
+    E2.FirstName || ' ' || E2.LastName AS SupervisorName
+FROM employees AS E1
+LEFT JOIN employees AS E2 ON E1.ReportsTo = E2.EmployeeId;
