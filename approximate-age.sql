d3a67082a0e9bfcdabd9786a77d227d0679ef361
Wiibleyde <nathan@bonnell.fr>
2023-10-23 14:11:45 +0200

    Date calculs...

--- approximate-age.sql
+++ approximate-age.sql
@@ -1,5 +1,3 @@
-SELECT FirstName, LastName,
-    (CAST(STRFTIME('%Y', 'now') AS INTEGER) - CAST(SUBSTR(BirthDate, 1, 4) AS INTEGER) -
-    CASE WHEN STRFTIME('%m-%d', 'now') < SUBSTR(BirthDate, 6, 5) THEN 1 ELSE 0 END) AS ApproximateAge
-FROM employees
+SELECT FirstName, LastName, strftime('%Y', HireDate) - strftime('%Y', BirthDate) AS ApproximateAge
+FROM Employees
 ORDER BY ApproximateAge ASC;
