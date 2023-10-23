36e53e5c23436b32d72c59c2a51d23ad25322b88
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-10-23 19:24:59 +0200

    list all country

--- count-all-country-from-tables.sql
+++ count-all-country-from-tables.sql
@@ -5,12 +5,12 @@ WITH AllCountries AS (
     UNION ALL
     SELECT BillingCountry AS Country FROM invoices
 )
+
 SELECT
     Country,
-    COUNT(*) AS Total,
+	COUNT(*) AS Total,
     (SELECT COUNT(*) FROM employees e WHERE e.Country = AllCountries.Country) AS Employees,
-    (SELECT COUNT(*) FROM customers c WHERE c.Country = AllCountries.Country) AS Customers,
+	(SELECT COUNT(*) FROM customers c WHERE c.Country = AllCountries.Country) AS Customers,
     (SELECT COUNT(*) FROM invoices i WHERE i.BillingCountry = AllCountries.Country) AS Invoices
 FROM AllCountries
-GROUP BY Country
-ORDER BY Country;
+GROUP BY Country ORDER BY Country;
