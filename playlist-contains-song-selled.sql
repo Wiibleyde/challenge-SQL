5e119121aa766d97a5ac5e835dd98ec3518efda1
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-10-23 19:38:16 +0200

    playlist

--- playlist-contains-song-selled.sql
+++ playlist-contains-song-selled.sql
@@ -5,9 +5,9 @@ SELECT
         (SUM(CASE WHEN il.InvoiceLineId IS NOT NULL THEN 1 ELSE 0 END) * 100.0) / COUNT(t.TrackId),
         2
     ) AS "% Song Sold Twice"
-FROM playlists pt
-JOIN playlist_track ptt ON pt.PlaylistId = ptt.PlaylistId
-JOIN tracks t ON ptt.TrackId = t.TrackId
-LEFT JOIN invoice_items il ON t.TrackId = il.TrackId
-GROUP BY PlaylistId, PlaylistName
-ORDER BY PlaylistId;
+FROM playlists AS pt
+JOIN playlist_track AS ptt ON pt.PlaylistId = ptt.PlaylistId
+JOIN tracks AS t ON ptt.TrackId = t.TrackId
+LEFT JOIN invoice_items AS il ON t.TrackId = il.TrackId
+GROUP BY pt.PlaylistId, pt.Name
+ORDER BY pt.PlaylistId;
