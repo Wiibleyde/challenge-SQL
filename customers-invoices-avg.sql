a95552226dcff971aa87fc91319a7a173f27af59
Wiibleyde <nathan@bonnell.fr>
2023-10-23 12:16:31 +0200

    avg

--- customers-invoices-avg.sql
+++ customers-invoices-avg.sql
@@ -1,4 +1,4 @@
-SELECT c.CustomerID, c.FirstName, c.LastName, AVG(i.Total) AS InvoicesAverage
+SELECT c.FirstName, c.LastName, AVG(i.Total) AS InvoicesAverage
 FROM Customers c
 JOIN Invoices i ON c.CustomerID = i.CustomerID
-GROUP BY c.CustomerID, c.FirstName, c.LastName ORDER BY c.FirstName ASC;
+GROUP BY c.FirstName, c.LastName ORDER BY c.FirstName ASC;
