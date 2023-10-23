000518304706ce61ffc2b9dc30e8a9a06c4bbb26
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-10-23 19:34:24 +0200

    list all country

--- playlist-contains-song-selled.sql
+++ playlist-contains-song-selled.sql
@@ -3,7 +3,7 @@ WITH PlaylistTrackSales AS (
         pt.PlaylistId,
         pt.Name AS PlaylistName,
         t.TrackId,
-        SUM(IFNULL(il.InvoiceLineId, 0)) AS SalesCount
+        SUM(CASE WHEN il.InvoiceLineId IS NOT NULL THEN 1 ELSE 0 END) AS SalesCount
     FROM playlists pt
     JOIN playlist_track ptt ON pt.PlaylistId = ptt.PlaylistId
     JOIN tracks t ON ptt.TrackId = t.TrackId
@@ -15,7 +15,7 @@ SELECT
     ps.PlaylistId,
     ps.PlaylistName,
     ROUND(
-        SUM(IF(ps.SalesCount >= 2, 1, 0)) * 100.0 / COUNT(ps.TrackId),
+        (SUM(CASE WHEN ps.SalesCount >= 2 THEN 1 ELSE 0 END) * 100.0) / COUNT(ps.TrackId),
         2
     ) AS "% Song Sold Twice"
 FROM PlaylistTrackSales ps
