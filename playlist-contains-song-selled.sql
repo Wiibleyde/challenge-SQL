2ed955c1b4dd00e603539be0a4d15797260bfbad
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-10-23 21:45:17 +0200

    playlist

--- playlist-contains-song-selled.sql
+++ playlist-contains-song-selled.sql
@@ -1,12 +1,18 @@
-SELECT
-    p.PlaylistId,
-    p.Name,
-    ROUND(
-        (COUNT(DISTINCT il.TrackId) * 100.0) / (SELECT COUNT(DISTINCT TrackId) FROM invoice_items),
-        4
-    ) AS "% song selled twice"
+SELECT 
+	p.PlaylistId,
+	p.Name,
+	IFNULL(ROUND((SelledTwiceByPlaylist*1.0 / NbSongPlaylist*1.0) * 100.0, 3),0)
 FROM playlists p
-LEFT JOIN playlist_track pt ON p.PlaylistId = pt.PlaylistId
-LEFT JOIN invoice_items il ON pt.TrackId = il.TrackId
-GROUP BY p.PlaylistId, p.Name
-ORDER BY p.PlaylistId;
+    LEFT JOIN 
+        (
+            (
+                SELECT PlaylistId, COUNT(*) AS SelledTwiceByPlaylist FROM (SELECT TrackId FROM (
+                    SELECT TrackId, COUNT(*) AS CountSell FROM invoice_items GROUP BY TrackId) WHERE CountSell=2
+                ) SelledTwice
+            INNER JOIN playlist_track pt ON pt.TrackId = SelledTwice.TrackId GROUP BY PlaylistId ORDER BY PlaylistId)
+        ) SelledTwiceByPlaylist ON SelledTwiceByPlaylist.PlaylistId=p.PlaylistId
+    LEFT JOIN 
+        (
+            SELECT PlaylistId, COUNT(*) AS NbSongPlaylist FROM playlist_track GROUP BY PlaylistId
+        ) NbSongPlaylist ON NbSongPlaylist.PlaylistId=p.PlaylistId
+GROUP BY p.PlaylistId
