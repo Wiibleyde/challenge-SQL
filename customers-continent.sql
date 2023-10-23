57d880acc8e052e272c57703023a37695cdf0448
Wiibleyde <nathan@bonnell.fr>
2023-10-23 14:53:23 +0200

    contnient

--- customers-continent.sql
+++ customers-continent.sql
@@ -1,8 +1,8 @@
-SELECT FirstName, LastName,Country,
+SELECT FirstName, LastName, Country,
     CASE
         WHEN Country IN ('United States', 'Canada', 'Mexico', 'Brazil', 'Argentina', 'Peru') THEN 'America'
         WHEN Country IN ('China', 'Japan', 'India', 'South Korea', 'Thailand') THEN 'Asia'
-        WHEN Country IN ('United Kingdom', 'Germany', 'France', 'Italy', 'Spain') THEN 'Europe'
+        WHEN Country IN ('United Kingdom', 'Germany', 'France', 'Italy', 'Spain', 'Norway') THEN 'Europe'
         WHEN Country IN ('Australia', 'New Zealand') THEN 'Oceania'
         ELSE 'Other'
     END AS Continent
