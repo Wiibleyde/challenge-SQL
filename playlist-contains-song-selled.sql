508d5866a5d63502c67a362b07619ae713f8cf6a
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-10-23 21:46:11 +0200

    playlist

--- playlist-contains-song-selled.sql
+++ playlist-contains-song-selled.sql
@@ -1,7 +1,7 @@
 SELECT 
 	p.PlaylistId,
 	p.Name,
-	IFNULL(ROUND((SelledTwiceByPlaylist*1.0 / NbSongPlaylist*1.0) * 100.0, 3),0)
+	IFNULL(ROUND((SelledTwiceByPlaylist*1.0 / NbSongPlaylist*1.0) * 100.0, 3),0) AS '% song selled twice'
 FROM playlists p
     LEFT JOIN 
         (
